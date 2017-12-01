15fc42c79a10b8d93006bfc6a99808f40a8064e7
bryanlampert <bryanlampert@gmail.com>
2017-12-01 00:47:13 +0000

    Adding the countdown exercise

---
+++ w1d4/closures/countdown.js
@@ -0,0 +1,30 @@
+var countdownGenerator = function (x) {  // var y = 0;
+  var strCd = "T-minus ";
+  var strBo = "Blast Off!";
+  var strGone = "Rockets already gone, bub!";
+  x++;
+
+  return function(y) {
+    x--;
+    if (x > 0) {
+      return strCd + x + "...";
+    } else if (x === 0) {
+      return strBo;
+    } else {
+      return strGone;
+    }
+  };
+
+
+
+};
+
+var countdown = countdownGenerator(3);
+
+
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
---
+++ w1d4/closures/loadedDie.js
@@ -0,0 +1,16 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var i = 0;
+
+  // console.log(die, "die")
+
+  return function() {
+    return list[i++];
+  };
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
